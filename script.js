1144ecf8f79a924dbeaf64a2034993f107f50417
Luiz Miguel <125470344+luizmiguelbarros@users.noreply.github.com>
2024-03-12 10:00:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+
+focoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+})
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+})
+
